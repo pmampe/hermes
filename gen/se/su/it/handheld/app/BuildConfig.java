7953211df47cc40a33dc9f194c20ffa891d24815
Joakim Lundin <joakim.lundin@su.se>
2012-09-18 12:17:54 +0200

    Initial app structure.

---
+++ gen/se/su/it/handheld/app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package se.su.it.handheld.app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/se/su/it/handheld/app/Hermes.java
@@ -0,0 +1,15 @@
+package se.su.it.handheld.app;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class Hermes extends DroidGap {
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    super.loadUrl("file:///android_asset/www/index.html");
+  }
+}
