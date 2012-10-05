994138476a73a4882bdd80b6b8af5b7c05758f12
Joakim Lundin <joakim.lundin@su.se>
2012-10-05 07:36:13 +0200

    script to publish to phonegap.

---
+++ publish_to_phonegap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APP_ID="219084"
+USER="utveckling@it.su.se"
+PASS=${PG_PASS}
+TOKEN_URL="https://build.phonegap.com/token"
+API_URL="https://build.phonegap.com/api/v1/apps"
+TAR_FILE="su.tar.gz"
+
+rm ${TAR_FILE}
+tar -C assets/ -czvf ${TAR_FILE} www/
+
+TOKEN=`curl -su "${USER}:${PASS}" -X POST -d "" https://build.phonegap.com/token | sed -E 's/.*:"([^"]+)".*/\1/'`
+
+curl -X PUT -F file=@${TAR_FILE} ${API_URL}/${APP_ID}?auth_token=$TOKEN
+
