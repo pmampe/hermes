5c10db6a26da85f3e2711e15f31e8bc82180647a
Joakim Lundin <joakim.lundin@su.se>
2012-10-31 09:15:45 +0100

    format

--- assets/www/map/js/views/app-view.js
+++ assets/www/map/js/views/app-view.js
@@ -1,17 +1,19 @@
-$(function() {
-	
+$(function () {
+
   var AppView = Backbone.View.extend({
 
-    el: $('#page-map'),
+    el:$('#page-map'),
 
-    initialize: function() {
+    initialize:function () {
       this.togglePoiType();
 
       this.campuses = new Campuses();
       this.campuses.on("reset", this.renderCampuses, this);
 
       this.campuses.fetch({
-        error: function() { alert("ERROR! Failed to fetch campuses.") }
+        error:function () {
+          alert("ERROR! Failed to fetch campuses.")
+        }
       });
 
       this.searchResults = new LocationSearchResult();
@@ -21,60 +23,66 @@ $(function() {
       this.locations.on("reset", this.resetLocations, this);
 
       this.locations.fetch({
-        error: function() { alert("ERROR! Failed to fetch locations.") }
+        error:function () {
+          alert("ERROR! Failed to fetch locations.")
+        }
       });
     },
 
-    events: {
-      "change #campus": "changeCampus",
-      "change #poiType": "showPOIs",
-      "click a[id=search_button]": "doSearch"
+    events:{
+      "change #campus":"changeCampus",
+      "change #poiType":"showPOIs",
+      "click a[id=search_button]":"doSearch"
     },
 
-    doSearch: function( event ){
+    doSearch:function (event) {
       var campus = this.campuses.get($("#campus").val());
-      var types = _.reject($('#poiType').val(), function(val){ return val == ""; });
+      var types = _.reject($('#poiType').val(), function (val) {
+        return val == "";
+      });
 
       this.searchResults.fetch({
-        data: {
-          q: $("#search_input").val().trim(),
-          campus: campus.get('name'),
-          types: types
+        data:{
+          q:$("#search_input").val().trim(),
+          campus:campus.get('name'),
+          types:types
         },
-        error: function() {alert("ERROR! Failed to fetch search results.")}
+        error:function () {
+          alert("ERROR! Failed to fetch search results.")
+        }
       });
     },
 
-    resetLocations: function() {
-        var mapDiv = $('#map_canvas');
+    resetLocations:function () {
+      var mapDiv = $('#map_canvas');
 
-        var pin = this.locations.pin;
+      var pin = this.locations.pin;
 
-        this.locations.each(function(item) {
-          var itemLocation = item.get("locations");
-          var itemText = "<div style='font: 12px/1.5 Verdana, sans-serif;color: #2A3333;text-shadow:none'>" +
-  	    	"<h3>" + item.get("text") + "</h3>" +
-  	    	"<a href='javascript://noop' onclick='window.MapView.getDirections(new google.maps.LatLng(" + itemLocation[0] + "," + itemLocation[1] + "))'>Directions to here</a>" +
-  	    "</div>";
-          var itemCampus = item.get("campus");
-          var itemType = item.get("type");
+      this.locations.each(function (item) {
+        var itemLocation = item.get("locations");
+        var itemText = "<div style='font: 12px/1.5 Verdana, sans-serif;color: #2A3333;text-shadow:none'>" +
+          "<h3>" + item.get("text") + "</h3>" +
+          "<a href='javascript://noop' onclick='window.MapView.getDirections(new google.maps.LatLng(" + itemLocation[0] + "," + itemLocation[1] + "))'>Directions to here</a>" +
+          "</div>";
+        var itemCampus = item.get("campus");
+        var itemType = item.get("type");
 
-          mapDiv.gmap('addMarker', {
-            'position': new google.maps.LatLng(itemLocation[0], itemLocation[1]),
-            'poiType': itemCampus + "." + itemType,
-            'visible': false,
-            'icon': pin
-          }).click(function() {
-              mapDiv.gmap('openInfoWindow', { content: itemText }, this);
+        mapDiv.gmap('addMarker', {
+          'position':new google.maps.LatLng(itemLocation[0], itemLocation[1]),
+          'poiType':itemCampus + "." + itemType,
+          'visible':false,
+          'icon':pin
+        }).click(function () {
+            mapDiv.gmap('openInfoWindow', { content:itemText }, this);
           });
-        });
-      },
+      });
+    },
 
-    renderCampuses: function() {
-      var template = _.template( $("#campus_template").html(), {
-        defaultOptionName: "Campus",
-        options: this.campuses.toJSON()
-      } );
+    renderCampuses:function () {
+      var template = _.template($("#campus_template").html(), {
+        defaultOptionName:"Campus",
+        options:this.campuses.toJSON()
+      });
 
       this.$el.find('#campus').replaceWith(template);
       this.$el.find('#campus').selectmenu();
@@ -82,11 +90,11 @@ $(function() {
       this.$el.trigger("refresh");
     },
 
-    renderLocations: function(locations) {
-      var template = _.template( $("#location_template").html(), {
-        defaultOptionName: "Filter",
-        options: locations
-      } );
+    renderLocations:function (locations) {
+      var template = _.template($("#location_template").html(), {
+        defaultOptionName:"Filter",
+        options:locations
+      });
 
       var types = $('#poiType').val();
 
@@ -107,7 +115,7 @@ $(function() {
       this.$el.trigger("refresh");
     },
 
-    changeCampus: function(e, v) {
+    changeCampus:function (e, v) {
       this.togglePoiType();
       var campus = this.campuses.get($("#campus").val());
       window.MapView.centerOnLocation(campus.get("coords"), campus.get("zoom"));
@@ -125,7 +133,7 @@ $(function() {
       $('#poiType').trigger("change"); // doesn't work from here for some reason..
     },
 
-    showPOIs: function() {
+    showPOIs:function () {
       var campus = this.campuses.get($("#campus").val());
       if (campus != null)
         campus = campus.get("name");
@@ -139,7 +147,7 @@ $(function() {
     },
 
     // disable poiType if no campus is chosen, else enable
-    togglePoiType: function() {
+    togglePoiType:function () {
       if ($("#campus").val() == "") {
         $('#poiType').selectmenu("disable");
       } else {
@@ -147,18 +155,18 @@ $(function() {
       }
     },
 
-    fadingMsg: function(locMsg) {
+    fadingMsg:function (locMsg) {
       $("<div class='ui-overlay-shadow ui-body-e ui-corner-all fading-msg'>" + locMsg + "</div>")
-        .css({ "display": "block", "opacity": 0.9, "top": $(window).scrollTop() + 100 })
-        .appendTo( $.mobile.pageContainer )
-        .delay( 2200 )
-        .fadeOut( 1000, function(){
+        .css({ "display":"block", "opacity":0.9, "top":$(window).scrollTop() + 100 })
+        .appendTo($.mobile.pageContainer)
+        .delay(2200)
+        .fadeOut(1000, function () {
           $(this).remove();
         });
     },
 
-    renderResultList: function() {
-    	window.MapView.renderResultList(this.searchResults);
+    renderResultList:function () {
+      window.MapView.renderResultList(this.searchResults);
     }
 
   }); //-- End of App view
