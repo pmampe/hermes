33cb3b79ea35fa46c7b59c859ff12bfdfd0f165e
Joakim Lundin <joakim.lundin@su.se>
2012-12-05 07:31:14 +0100

    Get locale from phonegap.

--- assets/www/map/js/main.js
+++ assets/www/map/js/main.js
@@ -1,11 +1,21 @@
 function startModule() {
-  // Init i18n, not setting language will cause i18next to try browser language.
-  i18n.init({
-    useCookie:false,
-    fallbackLng:'en',
-    resGetPath:'locales/__lng__.json',
-    getAsync:false
-  });
+  // Get locale from phonegap
+  var globalization = navigator.globalization;
+
+  if (globalization) {
+    navigator.globalization.getLocaleName(
+        function (locale) {
+          setLocale(locale.value);
+        },
+        function () {
+          console.log("Failed to get locale from phonegap. Using default.");
+          setLocale();
+        }
+    );
+  }
+  else {
+    setLocale();
+  }
 
   var appView = new AppView({ el:$('#page-map') });
   appView.render();
@@ -14,4 +24,19 @@ function startModule() {
   document.addEventListener("searchbutton", function () {
     this.openSearchPopup(null);
   }, false);
+}
+
+function setLocale(locale) {
+  var options = {
+    useCookie:false,
+    fallbackLng:'en',
+    resGetPath:'locales/__lng__.json',
+    getAsync:false
+  };
+
+  if (locale) {
+    options.locale = locale;
+  }
+
+  i18n.init(options);
 }
