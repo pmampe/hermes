acc3cd163b7e4a0cff199f7ba5eff670310a8974
Joakim Lundin <joakim.lundin@su.se>
2012-12-06 09:30:03 +0100

    Started using JSDoc a javaDoc equivalent for js.

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,16 +1,41 @@
+/**
+ * The app.
+ *
+ * @author <a href="mailto:joakim.lundin@su.se">Joakim Lundin</a>
+ * @author <a href="mailto:lucien.bokouka@su.se">Lucien Bokouka</a>
+ * @author <a href="mailto:henrik.dryselius@su.se">Henrik Dryselius</a>
+ *
+ * @class The app.
+ */
 var app = {
-  initialize:function () {
+  /**
+   * Initialize the app
+   */
+  initialize: function () {
     this.bind();
   },
-  bind:function () {
+
+  /**
+   * Bind listeners
+   */
+  bind: function () {
     document.addEventListener('deviceready', this.deviceready, false);
   },
-  deviceready:function () {
+
+  /**
+   * Callback function for deviceready event
+   */
+  deviceready: function () {
     // note that this is an event handler so the scope is that of the event
     // so we need to call app.report(), and not this.report()
     app.report('deviceready');
   },
-  report:function (id) {
+
+  /**
+   * Report
+   * @param id id of dom element to report on.
+   */
+  report: function (id) {
     // hide the .pending <p> and show the .complete <p>
     document.querySelector('#' + id + ' .pending').className += ' hide';
     var completeElem = document.querySelector('#' + id + ' .complete');
