3a59a3b55e94dfb977cc05a0144b45d19cccae35
Joakim Lundin <joakim.lundin@su.se>
2012-12-07 09:21:12 +0100

    Added tests for person model.

--- assets/www/spec/fixtures.js
+++ assets/www/spec/fixtures.js
@@ -2,58 +2,84 @@ beforeEach(function () {
 
   this.fixtures = {
 
-    Locations:{
-      valid:[
+    Locations: {
+      valid: [
         {
-          id:1,
-          campus:'Frescati',
-          type:'parkering',
-          subType:'mc',
-          shape:"line",
-          text:"Foobar",
-          coords:[
+          id: 1,
+          campus: 'Frescati',
+          type: 'parkering',
+          subType: 'mc',
+          shape: "line",
+          text: "Foobar",
+          coords: [
             [59.00, 18.00]
           ],
-          directionAware:false
+          directionAware: false
         },
         {
-          id:2,
-          campus:'Frescati',
-          type:'parkering',
-          subType:'mc',
-          shape:"line",
-          text:"Foobar",
-          coords:[
+          id: 2,
+          campus: 'Frescati',
+          type: 'parkering',
+          subType: 'mc',
+          shape: "line",
+          text: "Foobar",
+          coords: [
             [59.00, 18.00]
           ],
-          directionAware:false
+          directionAware: false
         },
         {
-          id:3,
-          campus:'Frescati',
-          type:'hörsal',
-          subType:'mc',
-          shape:"line",
-          text:"Foobar",
-          coords:[
+          id: 3,
+          campus: 'Frescati',
+          type: 'hörsal',
+          subType: 'mc',
+          shape: "line",
+          text: "Foobar",
+          coords: [
             [59.00, 18.00]
           ],
-          directionAware:false
+          directionAware: false
         },
         {
-          id:4,
-          campus:'Kista',
-          type:'parkering',
-          subType:'mc',
-          shape:"line",
-          text:"Foobar",
-          coords:[
+          id: 4,
+          campus: 'Kista',
+          type: 'parkering',
+          subType: 'mc',
+          shape: "line",
+          text: "Foobar",
+          coords: [
             [59.00, 18.00]
           ],
-          directionAware:false
+          directionAware: false
+        }
+      ]
+    },
+
+    Persons: {
+      valid: [
+        {
+          "id": "test1",
+          "uid": "test1",
+          "displayName": "Test1 Räksmörgås",
+          "mail": "test1@su.se",
+          "telephoneNumber": null,
+          "sn": "Räksmörgås",
+          "cn": "Test1 Räksmörgås",
+          "givenName": "Test1",
+          "eduPersonPrincipalName": "test1@su.se"
+        },
+        {
+          "id": "test4",
+          "uid": "test4",
+          "displayName": "Fernandol Torresson",
+          "mail": "test444@fraita.su.se",
+          "telephoneNumber": "+55543335950",
+          "sn": "Torresson",
+          "cn": "Fernandol Torresson",
+          "givenName": "Fernandol",
+          "eduPersonPrincipalName": "test4@su.se"
         }
       ]
     }
   };
-
 });
---
+++ assets/www/spec/sukat.spec.js
@@ -0,0 +1,89 @@
+describe('Person model', function () {
+  describe('when creating an empty person', function () {
+    beforeEach(function () {
+      this.person = new Person();
+    });
+
+    it('should have givenName Unknown', function () {
+      expect(this.person.get('givenName')).toEqual("Unknown");
+    });
+
+    it('should have sn Unknown', function () {
+      expect(this.person.get('sn')).toEqual("Unknown");
+    });
+
+    it('should have displayName Unknown', function () {
+      expect(this.person.get('displayName')).toEqual("Unknown");
+    });
+
+    it('should have no mail address', function () {
+      expect(this.person.get('mail')).toEqual("");
+    });
+
+    it('should have telephoneNumber as the phone exchange number', function () {
+      expect(this.person.get('telephoneNumber')).toEqual("+468162000");
+    });
+  });
+});
+
+describe('Person collection', function () {
+  describe('creating an empty collection', function () {
+    beforeEach(function () {
+      this.persons = new Persons();
+    });
+
+    it('should have Person for model', function () {
+      expect(this.persons.model).toBe(Person);
+    });
+
+    it('should have a url pointing at broker geo api', function () {
+      expect(this.persons.url()).toMatch(/http:\/\/.+\.su\.se\/sukat\/.+/);
+    });
+  });
+
+  describe('fetching a collection of persons', function () {
+    beforeEach(function () {
+      this.persons = new Persons();
+      this.fixture = this.fixtures.Persons.valid;
+
+      this.server = sinon.fakeServer.create();
+      this.server.respondWith(
+          "GET",
+          this.persons.url(),
+          this.validResponse(this.fixture)
+      );
+    });
+
+    afterEach(function () {
+      this.server.restore();
+    });
+
+    it('should make a correct request', function () {
+      this.persons.fetch();
+      expect(this.server.requests.length).toEqual(1);
+      expect(this.server.requests[0].method).toEqual("GET");
+      expect(this.server.requests[0].url).toMatch(/.*\/search/);
+    });
+
+    it('should return all persons', function () {
+      this.persons.fetch();
+      this.server.respond();
+      expect(this.persons.length).toEqual(2);
+    });
+
+    it('should override defaults', function () {
+      this.persons.fetch();
+      this.server.respond();
+      var firstPerson = this.persons.get("test1");
+      expect(firstPerson.get('id')).toEqual("test1");
+      expect(firstPerson.get('uid')).toEqual('test1');
+      expect(firstPerson.get('displayName')).toEqual('Test1 Räksmörgås');
+      expect(firstPerson.get('mail')).toEqual('test1@su.se');
+      expect(firstPerson.get('telephoneNumber')).toBeNull();
+      expect(firstPerson.get('sn')).toEqual('Räksmörgås');
+      expect(firstPerson.get('cn')).toEqual('Test1 Räksmörgås');
+      expect(firstPerson.get('givenName')).toEqual('Test1');
+      expect(firstPerson.get('eduPersonPrincipalName')).toEqual('test1@su.se');
+    });
+  });
+});
