95919f611fda43f4b80ed345ec531824fd30ce63
Lucien Bokouka <lucien.bokouka@su.se>
2013-01-29 17:05:33 +0100

    updated childbrowser & only fiddle with plugins if we have em loaded. (from 53cd57dbdb0b09dd3573c3dc648fa5c1e2f4c4e8)

--- assets/www/childbrowser.js
+++ assets/www/childbrowser.js
@@ -1,92 +1,121 @@
 /*
- * cordova is available under *either* the terms of the modified BSD license *or* the
+ * PhoneGap is available under *either* the terms of the modified BSD license *or* the
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
  *
+ * Copyright 2012, Andrew Lunny, Adobe Systems
  * Copyright (c) 2005-2010, Nitobi Software Inc.
  * Copyright (c) 2011, IBM Corporation
+ * (c) 2010 Jesse MacFadyen, Nitobi
  */
 
-/**
- * Constructor
- */
-function ChildBrowser() {
-};
+var ChildBrowser = (function (gap) {
+  function isFunction(f) {
+    return typeof f === "function";
+  }
 
-ChildBrowser.CLOSE_EVENT = 0;
-ChildBrowser.LOCATION_CHANGED_EVENT = 1;
+  // placeholder and constants
+  function ChildBrowser() {
+  }
 
-/**
- * Display a new browser with the specified URL.
- * This method loads up a new web view in a dialog.
- *
- * @param url           The url to load
- * @param options       An object that specifies additional options
- */
-ChildBrowser.prototype.showWebPage = function(url, options) {
-    options = options || {
-        showLocationBar: true,
-        locationBarAlign: "top"
-    };
-    cordova.exec(this._onEvent, this._onError, "ChildBrowser", "showWebPage", [url, options]);
-};
+  var CLOSE_EVENT = 0,
+      LOCATION_CHANGED_EVENT = 1,
+      OPEN_EXTERNAL_EVENT = 2;
 
-/**
- * Close the browser opened by showWebPage.
- */
-ChildBrowser.prototype.close = function() {
-    cordova.exec(null, null, "ChildBrowser", "close", []);
-};
+  /**
+   * Function called when the child browser has an event.
+   */
+  function onEvent(data) {
+    switch (data.type) {
+      case CLOSE_EVENT:
+        if (isFunction(ChildBrowser.onClose)) {
+          ChildBrowser.onClose();
+        }
+        break;
+      case LOCATION_CHANGED_EVENT:
+        if (isFunction(ChildBrowser.onLocationChange)) {
+          ChildBrowser.onLocationChange(data.location);
+        }
+        break;
+      case OPEN_EXTERNAL_EVENT:
+        if (isFunction(ChildBrowser.onOpenExternal)) {
+          ChildBrowser.onOpenExternal();
+        }
+        break;
+    }
+  }
 
-/**
- * Display a new browser with the specified URL.
- * This method starts a new web browser activity.
- *
- * @param url           The url to load
- * @param usecordova   Load url in cordova webview [optional]
- */
-ChildBrowser.prototype.openExternal = function(url, usecordova) {
-    if (usecordova === true) {
+  /**
+   * Function called when the child browser has an error.
+   */
+  function onError(data) {
+    if (isFunction(ChildBrowser.onError)) {
+      ChildBrowser.onError(data);
+    }
+  }
+
+  /**
+   * Maintain API consistency with iOS
+   */
+  ChildBrowser.install = function () {
+    console.log('ChildBrowser.install is deprecated');
+  }
+
+  /**
+   * Display a new browser with the specified URL.
+   * This method loads up a new web view in a dialog.
+   *
+   * @param url           The url to load
+   * @param options       An object that specifies additional options
+   */
+  ChildBrowser.showWebPage = function (url, options) {
+    if (!options) {
+      options = { showLocationBar:true };
+    }
+
+    gap.exec(onEvent, onError, "ChildBrowser", "showWebPage", [url, options]);
+  };
+
+  /**
+   * Close the browser opened by showWebPage.
+   */
+  ChildBrowser.close = function () {
+    gap.exec(null, null, "ChildBrowser", "close", []);
+  };
+
+  /**
+   * Display a new browser with the specified URL.
+   * This method starts a new web browser activity.
+   *
+   * @param url           The url to load
+   * @param usePhoneGap   Load url in PhoneGap webview [optional]
+   */
+  ChildBrowser.openExternal = function (url, usePhoneGap) {
+    if (device.platform.toLowerCase() == 'android') {
+      if (usePhoneGap) {
         navigator.app.loadUrl(url);
+      } else {
+        gap.exec(null, null, "ChildBrowser", "openExternal", [url, usePhoneGap]);
+      }
+    } else {
+      ChildBrowser.showWebPage(url);
     }
-    else {
-        cordova.exec(null, null, "ChildBrowser", "openExternal", [url, usecordova]);
+    ;
+  };
+
+  /**
+   * Load ChildBrowser
+   */
+  gap.addConstructor(function () {
+    if (gap.addPlugin) {
+      gap.addPlugin("childBrowser", ChildBrowser);
+    } else {
+      if (!window.plugins) {
+        window.plugins = {};
+      }
+
+      window.plugins.childBrowser = ChildBrowser;
     }
-};
+  });
 
-/**
- * Method called when the child browser has an event.
- */
-ChildBrowser.prototype._onEvent = function(data) {
-    if (data.type == ChildBrowser.CLOSE_EVENT && typeof window.plugins.childBrowser.onClose === "function") {
-        window.plugins.childBrowser.onClose();
-    }
-    if (data.type == ChildBrowser.LOCATION_CHANGED_EVENT && typeof window.plugins.childBrowser.onLocationChange === "function") {
-        window.plugins.childBrowser.onLocationChange(data.location);
-    }
-};
-
-/**
- * Method called when the child browser has an error.
- */
-ChildBrowser.prototype._onError = function(data) {
-    if (typeof window.plugins.childBrowser.onError === "function") {
-        window.plugins.childBrowser.onError(data);
-    }
-};
-
-/**
- * Maintain API consistency with iOS
- */
-ChildBrowser.prototype.install = function(){
-};
-
-/**
- * Load ChildBrowser
- */
-
-if(!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.childBrowser) {
-    window.plugins.childBrowser = new ChildBrowser();
-}
+  return ChildBrowser;
+})(window.cordova || window.Cordova || window.PhoneGap);
