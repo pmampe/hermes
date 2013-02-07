312e00ece516d68151cec9fabe38e16c8867191a
Joakim Lundin <joakim.lundin@su.se>
2013-02-07 08:07:40 +0100

    Fixed violations.

--- assets/www/map/js/views/campus-popup-view.js
+++ assets/www/map/js/views/campus-popup-view.js
@@ -6,69 +6,70 @@
  * @type {Backbone.View}
  */
 var CampusPopupView = Backbone.View.extend(
-	/** @lends CampusPopupView */
-	{
+    /** @lends CampusPopupView */
+    {
 
-		/**
-		 * @constructs
-		 * @param campuses A list of campuses to display.
-		 */
-		initialize: function (options) {
-			_.bindAll(this, "render");
+      /**
+       * @constructs
+       * @param campuses A list of campuses to display.
+       */
+      initialize:function (options) {
+        _.bindAll(this, "render");
 
-			this.campuses = options.campuses;
-			this.campusesMap = options.campusesMap;
-		},
-
-
-
-		/** Registers events */
-		events: {
-		  "click #campusesPopupList": "selectCampus",
-		},
-
-		selectCampus: function(evt) {
-			// get the campus id from the parent <li> (format "campus-X", where X is a number)
-			var campusId = $(evt.target).parents("li").get(0).id.split("campus-")[1];
-			$("#campus").val(campusId);
-			$("#campus").selectmenu("refresh");
-			$("#campus").trigger("change");
-		},
-
-		/**
-		 * Render the campus popup view.
-		 */
-		render: function () {
-			var self = this; 
-			// remove everything from the list except the first element (the header)
-			$("#campusesPopupList").find("li:not(:first-child)").remove();
-			$(this.campuses).each(function(i, campusName) { // append all campuses
-				$("#campusesPopupList").append("<li id='campus-" + self.campusesMap[campusName] + "'><a href='javascript://nop'>" + campusName + "</a></li>");
-			});
-			$("#campusesPopupList").listview("refresh"); // jQuery mobile-ify the added elements
+        this.campuses = options.campuses;
+        this.campusesMap = options.campusesMap;
+      },
 
 
-			// close any other open popup (only one popup can be open at the same time.)
-			$(document).find("[data-role='popup']:not([id='campusesPopup'])").popup("close").bind({
-				// show the campuses popup after the other popups has closed
-				popupafterclose: function(event, uit) {
-          // --- HACK: for correct x and y position of popup ---
-          var x = 150;
-          // device is only defined when running on a physical device (not a web browser)
-          if (typeof device != 'undefined') x = 50; 
-          
-          var cellHeight = $("#campusesPopup li:nth-child(2)").height();
-          var noOfCells = $("#campusesPopup li:not(:first-child)").length;
-          var y = noOfCells * (cellHeight / 3) + 150;
-          // --- END  HACK: for correct x and y position of popup ---
+      /** Registers events */
+      events:{
+        "click #campusesPopupList":"selectCampus"
+      },
 
-          $( "#campusesPopup" ).popup( "open", { x:x, y:y } );
-					
-					// unbind popupafterclose event after campuses popup open, to ensure it's 
-					// not accidentally opened after any other search.
-					$(document).find("[data-role='popup']:not([id='campusesPopup'])").unbind("popupafterclose");
-				}
-			});
-		}
+      selectCampus:function (evt) {
+        // get the campus id from the parent <li> (format "campus-X", where X is a number)
+        var campusId = $(evt.target).parents("li").get(0).id.split("campus-")[1];
+        $("#campus").val(campusId);
+        $("#campus").selectmenu("refresh");
+        $("#campus").trigger("change");
+      },
 
-	}); 
+      /**
+       * Render the campus popup view.
+       */
+      render:function () {
+        var self = this;
+        // remove everything from the list except the first element (the header)
+        $("#campusesPopupList").find("li:not(:first-child)").remove();
+        $(this.campuses).each(function (i, campusName) { // append all campuses
+          $("#campusesPopupList").append("<li id='campus-" + self.campusesMap[campusName] + "'><a href='javascript://nop'>" + campusName + "</a></li>");
+        });
+        $("#campusesPopupList").listview("refresh"); // jQuery mobile-ify the added elements
+
+
+        // close any other open popup (only one popup can be open at the same time.)
+        $(document).find("[data-role='popup']:not([id='campusesPopup'])").popup("close").bind({
+          // show the campuses popup after the other popups has closed
+          popupafterclose:function (event, uit) {
+            // --- HACK: for correct x and y position of popup ---
+            var x = 150;
+            // device is only defined when running on a physical device (not a web browser)
+            if (typeof device != 'undefined') {
+              x = 50;
+            }
+
+            var cellHeight = $("#campusesPopup li:nth-child(2)").height();
+            var noOfCells = $("#campusesPopup li:not(:first-child)").length;
+            var y = noOfCells * (cellHeight / 3) + 150;
+            // --- END  HACK: for correct x and y position of popup ---
+
+            $("#campusesPopup").popup("open", { x:x, y:y });
+
+            // unbind popupafterclose event after campuses popup open, to ensure it's
+            // not accidentally opened after any other search.
+            $(document).find("[data-role='popup']:not([id='campusesPopup'])").unbind("popupafterclose");
+          }
+        });
+      }
+
+    });
