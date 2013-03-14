f2cd4982c340935f1285dc274dd33084da3d3ba0
Joakim Lundin <joakim.lundin@su.se>
2013-03-14 07:27:48 +0100

    missing ;

--- assets/www/map/js/views/point-location-view.js
+++ assets/www/map/js/views/point-location-view.js
@@ -14,17 +14,17 @@ var PointLocationView = GenericLocationView.extend(
        * @param options options for this view.
        */
       initialize: function (options) {
-        
+
         var pin = options.model.get('pin');
         var position = this.getPosition(options);
-        
+
         // if the model contains customised icon, show it instead of the default one.
         if (options.model.get('hasIcon')) {
           var locationId = options.model.get('id');
           pin = new google.maps.MarkerImage(
-              'http://pgbroker-dev.it.su.se/image/view/' + locationId, 
+              'http://pgbroker-dev.it.su.se/image/view/' + locationId,
               new google.maps.Size(22, 22));
-        } 
+        }
 
         options.marker = new google.maps.Marker({
           position: position,
@@ -44,20 +44,20 @@ var PointLocationView = GenericLocationView.extend(
 
         this.constructor.__super__.initialize.apply(this, [options]);
       },
-      
+
       /**
-       * getPosition checks if the default position stored in the model is overridden 
+       * getPosition checks if the default position stored in the model is overridden
        * by the customizedPosition parameter. If it is it the customizedPosition, else
        * use the location stored in the model.
        */
-      getPosition : function(options) {
+      getPosition: function (options) {
         var position;
         if (options.customizedPosition) {
           position = options.customizedPosition;
         } else {
-          position = _.flatten(options.model.getGPoints())[0]
+          position = _.flatten(options.model.getGPoints())[0];
         }
-        
+
         return position;
       },
 
