bf4b5c33ed9833deacfc706290c704b2b26b6f7b
Joakim Lundin <joakim.lundin@su.se>
2013-04-03 10:01:29 +0200

    MOB-41 - Started rebuilding start page grid.

--- assets/www/js/views/app-view.js
+++ assets/www/js/views/app-view.js
@@ -1,11 +1,10 @@
 var AppView = Backbone.View.extend({
-  events:{
-    "click a[id=mondo-link]":"openInAppBrowser",
-    "click a[id=su-link]":"openInAppBrowser",
-    "click a[id=sub-link]":"openInAppBrowser"
+  events: {
+    "click a[id=servicelink]": "openInAppBrowser",
+    "click a[id=sisulink]": "openInAppBrowser"
   },
 
-  openInAppBrowser:function (event) {
+  openInAppBrowser: function (event) {
     event.preventDefault();
     var url = event.currentTarget.href;
     window.open(url, '_blank', 'location=yes');
