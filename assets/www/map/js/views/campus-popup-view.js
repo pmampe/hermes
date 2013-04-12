2ae0c421ba52450005dfb9b1294812c458e7bc83
Joakim Lundin <joakim.lundin@su.se>
2013-04-12 06:58:18 +0200

    MOB-128 - Initialize the listview before trying to refresh it.

--- assets/www/map/js/views/campus-popup-view.js
+++ assets/www/map/js/views/campus-popup-view.js
@@ -13,7 +13,7 @@ var CampusPopupView = Backbone.View.extend(
        * @constructs
        * @param campuses A list of campuses to display.
        */
-      initialize:function (options) {
+      initialize: function (options) {
         _.bindAll(this, "render");
 
         this.campuses = options.campuses;
@@ -22,11 +22,11 @@ var CampusPopupView = Backbone.View.extend(
 
 
       /** Registers events */
-      events:{
-        "click #campusesPopupList":"selectCampus"
+      events: {
+        "click #campusesPopupList": "selectCampus"
       },
 
-      selectCampus:function (evt) {
+      selectCampus: function (evt) {
         // get the campus id from the parent <li> (format "campus-X", where X is a number)
         var campusId = $(evt.target).parents("li").get(0).id.split("campus-")[1];
         $("#campus").val(campusId);
@@ -37,20 +37,21 @@ var CampusPopupView = Backbone.View.extend(
       /**
        * Render the campus popup view.
        */
-      render:function () {
+      render: function () {
         var self = this;
         // remove everything from the list except the first element (the header)
         $("#campusesPopupList").find("li:not(:first-child)").remove();
         $(this.campuses).each(function (i, campusName) { // append all campuses
           $("#campusesPopupList").append("<li id='campus-" + self.campusesMap[campusName] + "'><a href='javascript://nop'>" + campusName + "</a></li>");
         });
+        $("#campusesPopupList").listview();
         $("#campusesPopupList").listview("refresh"); // jQuery mobile-ify the added elements
 
 
         // close any other open popup (only one popup can be open at the same time.)
         $(document).find("[data-role='popup']:not([id='campusesPopup'])").popup("close").bind({
           // show the campuses popup after the other popups has closed
-          popupafterclose:function (event, uit) {
+          popupafterclose: function (event, uit) {
             // --- HACK: for correct x and y position of popup ---
             var x = 150;
             // device is only defined when running on a physical device (not a web browser)
@@ -63,7 +64,7 @@ var CampusPopupView = Backbone.View.extend(
             var y = noOfCells * (cellHeight / 3) + 150;
             // --- END  HACK: for correct x and y position of popup ---
 
-            $("#campusesPopup").popup("open", { x:x, y:y });
+            $("#campusesPopup").popup("open", { x: x, y: y });
 
             // unbind popupafterclose event after campuses popup open, to ensure it's
             // not accidentally opened after any other search.
