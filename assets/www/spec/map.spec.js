77b06fe75252446ff9885a970258704a38e9cb62
Joakim Lundin <joakim.lundin@su.se>
2013-04-12 07:35:44 +0200

    MOB-128 - Fixed broken tests.

--- assets/www/spec/map.spec.js
+++ assets/www/spec/map.spec.js
@@ -263,25 +263,25 @@ describe('Map model', function () {
 describe('Map view', function () {
   beforeEach(function () {
     var html = "<div data-role='page' id='page-map' style='width:200px; height:200px'>" +
-                 "<div id='search-box' class='ui-mini'>" +
-                   "<ul id='search-autocomplete' " +
-                        "data-role='listview' " +
-                        "data-theme='a' " +
-                        "data-filter-theme='a' " +
-                        "data-mini='true' " +
-                        "data-filter-mini='true' " +
-                        "data-filter='true' " +
-                        "data-filter-placeholder='Enter search string' " +
-                        "data-autodividers='true' " +
-                        "data-inset= 'true'>" +
-                   "</ul>" +
-                 "</div>" +
-                 "<div id='map_canvas'></div>" +
-               "</div>";
+        "<div id='search-box' class='ui-mini'>" +
+        "<ul id='search-autocomplete' " +
+        "data-role='listview' " +
+        "data-theme='a' " +
+        "data-filter-theme='a' " +
+        "data-mini='true' " +
+        "data-filter-mini='true' " +
+        "data-filter='true' " +
+        "data-filter-placeholder='Enter search string' " +
+        "data-autodividers='true' " +
+        "data-inset= 'true'>" +
+        "</ul>" +
+        "</div>" +
+        "<div id='map_canvas'></div>" +
+        "</div>";
 
     $('#stage').replaceWith(html);
     $.mobile.loadPage("#page-map");
-    
+
     this.view = new MapView({el: $('#map_canvas')});
   });
 
@@ -297,7 +297,7 @@ describe('Map view', function () {
   });
 
   describe('resize', function () {
-    beforeEach(function() {
+    beforeEach(function () {
       // We need to create a new view since we need to attach the spy first
       spyOn(MapView.prototype, 'resize');
       this.view = new MapView({el: $('#map_canvas')});
--- assets/www/spec/searchview.spec.js
+++ assets/www/spec/searchview.spec.js
@@ -2,86 +2,59 @@ describe('Map views search filter', function () {
   beforeEach(function () {
     var html = "<div data-role='page' id='page-map' style='width:200px; height:200px'>" +
         "<div id='search-box' class='ui-mini'>" +
-          "<ul id='search-autocomplete' " +
-               "data-role='listview' " +
-               "data-theme='a' " +
-               "data-filter-theme='a' " +
-               "data-mini='true' " +
-               "data-filter-mini='true' " +
-               "data-filter='true' " +
-               "data-filter-placeholder='Enter search string' " +
-               "data-autodividers='true' " +
-               "data-inset= 'true'>" +
-          "</ul>" +
+        "<ul id='search-autocomplete' " +
+        "data-role='listview' " +
+        "data-theme='a' " +
+        "data-filter-theme='a' " +
+        "data-mini='true' " +
+        "data-filter-mini='true' " +
+        "data-filter='true' " +
+        "data-filter-placeholder='Enter search string' " +
+        "data-autodividers='true' " +
+        "data-inset= 'true'>" +
+        "</ul>" +
         "</div>" +
         "<div id='map_canvas'></div>" +
-      "</div>";
-    
+        "</div>";
+
     $('#stage').replaceWith(html);
     $.mobile.loadPage("#page-map");
-    
-    
-    this.locations = new Locations();
-    this.fixture = this.fixtures.Locations.valid;
 
     this.server = sinon.fakeServer.create();
     this.server.respondWith(
         "GET",
-        this.locations.url(),
-        this.validResponse(this.fixture)
+        Locations.prototype.url(),
+        this.validResponse(this.fixtures.Locations.valid)
     );
-    
-    
-    this.appView = new AppView({ el: $('#page-map'), title: "Hör- & skrivsalar" });
-    this.appView.render();
-    this.appView.showType("auditorium");
-    
-    this.server.respond();
-
-    
-    this.searchView = this.appView.mapView.searchView;
   });
-  
+
   afterEach(function () {
     $('#page-map').replaceWith("<div id='stage'></div>");
     this.server.restore();
   });
-  
+
   describe('instantiation', function () {
-    it('should initialize', function () {
-      spyOn(this.searchView, "initialize");
-      new AppView({ el: $('#page-map'), title: "Hör- & skrivsalar" });
-      expect(this.searchView.initialize).toHaveBeenCalled();
-    });
-    
-    it('should render', function () {
-      spyOn(this.searchView, "render");
-      this.appView.showType("auditorium");
-      this.server.respond();
-      expect(this.searchView.render).toHaveBeenCalled();
-    });
-  });
-
-/*
-  //maryam tries some stuff
-  describe('populate filter list', function () {
-    beforeEach(function () {
-      var filteredList = '<div id="search-box" class="ui-mini">'+
-        '<ul id="search-autocomplete" data-role="listview" data-theme="a" data-filter-theme="a" data-mini="true" data-filter-mini="true"' +
-        'data-filter="true" data-filter-reveal="true" data-filter-placeholder="Enter search string" data-inset= "true">' +
-        '</ul>' +
-        '</div>';
-      $('#page-map').append(filteredList);
-      $.mobile.loadPage("#page-map");
+    it('should initialize search view on map view initialization', function () {
+      spyOn(SearchView.prototype, "initialize");
+      new MapView({ el: $('#map_canvas') });
+      expect(SearchView.prototype.initialize).toHaveBeenCalled();
     });
 
-    it('should populate filter list with the correct number of items', function () {
-      expect($("#search-autocomplete li").length).toEqual(0);
-      var list = ["E 144", "D4 a", "Hiss E3"];
-      this..filterSearch(list);
-      expect($("#search-autocomplete li").length).toEqual(3);
+    it('should render search view on location refresh', function () {
+      var mapView = new MapView({ el: $('#map_canvas') });
+      spyOn(mapView.searchView, "render");
+      runs(function () {
+        mapView.locations.fetch();
+        this.server.respond();
+      });
+
+      waitsFor(function () {
+        return mapView.locations.length > 0;
+      }, "Waiting for returning call", 1000);
+
+      runs(function () {
+        expect(mapView.searchView.render).toHaveBeenCalled();
+      });
     });
   });
-  */
-  //end
 });
