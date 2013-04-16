a184875d1c31fc00124bd6dfe787863ed008523a
Joakim Lundin <joakim.lundin@su.se>
2013-04-16 07:03:17 +0200

    Test the setMapPosition in map model.

--- assets/www/spec/map.spec.js
+++ assets/www/spec/map.spec.js
@@ -256,6 +256,22 @@ describe('Map model', function () {
       expect(this.model.get('location').lat()).toEqual(40);
       expect(this.model.get('location').lng()).toEqual(50);
     });
+
+    it('should have a default mapPosition', function () {
+      expect(this.model.get('mapPosition')).toBeDefined();
+      expect(this.model.get('mapPosition').lat()).toBeDefined();
+      expect(this.model.get('mapPosition').lng()).toBeDefined();
+    });
+  });
+
+  describe('setMapPosition', function () {
+    it('should update the mapPosition', function () {
+      this.model = new MapModel({ mapPosition: new google.maps.LatLng(0, 0) });
+      this.model.setMapPosition(10, 20);
+
+      expect(this.model.get('mapPosition').lat()).toEqual(10);
+      expect(this.model.get('mapPosition').lng()).toEqual(20);
+    });
   });
 });
 
