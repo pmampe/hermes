fb01fea35dcb03906afe0550ca51ec59c6db0781
Maryam Ammouri <maryam.ammouri@xlent.se>
2013-05-17 14:59:48 +0200

    MOB-165 Flickering home icon -  css icon fix text

--- assets/www/js/jst/common.js
+++ assets/www/js/jst/common.js
@@ -39,7 +39,7 @@ JST['common/header'] = _.template(" \
     <% if (backbutton) { %>\
       <a data-role='button' rel='external' data-ajax='false' data-transition='fade' data-rel='back' \
          class='ui-btn-left backbutton'> \
-         <div id='object-container' class='object-container'></div>\
+         <div id='home' class='home'></div>\
       </a> \
     <% } %> \
 \
