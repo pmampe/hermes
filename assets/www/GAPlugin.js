f0ef0cd3a951fba9046c342edbcb5d0da4184262
Joakim Lundin <joakim.lundin@su.se>
2013-05-23 09:05:49 +0200

    Upgraded to cordova v2.7.0 & GAPlugin 2.0.1

--- assets/www/GAPlugin.js
+++ assets/www/GAPlugin.js
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2013, IT Services, Stockholm University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of Stockholm University nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 (function () {
   var cordovaRef = window.PhoneGap || window.Cordova || window.cordova;
 
@@ -34,15 +65,11 @@
   // the next event only. If there is an existing custom variable at the specified
   // index, it will be overwritten by this one.
   //
-  // key = the name of the variable you are logging
   // value = the value of the variable you are logging
-  // index = the numerical index of the key / value pair (base 1)
-  //  Standard accounts support up to 5 custom variables, while premium
-  //  accounts support up to 50. The SDK will accept custom
-  //  variable indices higher than 5, but unless the account is a premium account,
-  //  those custom variables will not be processed.
-  GAPlugin.prototype.setVariable = function (success, fail, key, value, index) {
-    return cordovaRef.exec(success, fail, 'GAPlugin', 'setVariable', [key, value, index]);
+  // index = the numerical index of the dimension to which this variable will be assigned (1 - 20)
+  //  Standard accounts support up to 20 custom dimensions.
+  GAPlugin.prototype.setVariable = function (success, fail, index, value) {
+    return cordovaRef.exec(success, fail, 'GAPlugin', 'setVariable', [index, value]);
   };
 
   GAPlugin.prototype.exit = function (success, fail) {
--- src/com/adobe/plugins/GAPlugin.java
+++ src/com/adobe/plugins/GAPlugin.java
@@ -1,56 +1,110 @@
+/*
+ * Copyright (c) 2013, IT Services, Stockholm University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of Stockholm University nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.adobe.plugins;
 
-import com.google.android.apps.analytics.GoogleAnalyticsTracker;
-
-import org.apache.cordova.api.Plugin;
-import org.apache.cordova.api.PluginResult;
-import org.apache.cordova.api.PluginResult.Status;
-
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
 import org.json.JSONArray;
 
-public class GAPlugin extends Plugin {
-  @Override
-  public PluginResult execute(String action, JSONArray args, String callbackId) {
+import com.google.analytics.tracking.android.GAServiceManager;
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
 
-    GoogleAnalyticsTracker tracker = GoogleAnalyticsTracker.getInstance();
-    PluginResult result = null;
+public class GAPlugin extends CordovaPlugin {
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callback) {
+    GoogleAnalytics ga = GoogleAnalytics.getInstance(cordova.getActivity());
+    Tracker tracker = ga.getDefaultTracker();
 
     if (action.equals("initGA")) {
       try {
-        tracker.startNewSession(args.getString(0), args.getInt(1), this.cordova.getActivity());
-        result = new PluginResult(Status.OK, "initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
-      } catch (Exception e) {
-        result = new PluginResult(Status.JSON_EXCEPTION);
+        tracker = ga.getTracker(args.getString(0));
+        GAServiceManager.getInstance().setDispatchPeriod(args.getInt(1));
+        ga.setDefaultTracker(tracker);
+        callback.success("initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
+        return true;
+      } catch (final Exception e) {
+        callback.error(e.getMessage());
       }
     } else if (action.equals("exitGA")) {
       try {
-        tracker.stopSession();
-        result = new PluginResult(Status.OK, "exitGA");
-      } catch (Exception e) {
-        result = new PluginResult(Status.JSON_EXCEPTION);
+        GAServiceManager.getInstance().dispatch();
+        callback.success("exitGA");
+        return true;
+      } catch (final Exception e) {
+        callback.error(e.getMessage());
       }
     } else if (action.equals("trackEvent")) {
       try {
-        tracker.trackEvent(args.getString(0), args.getString(1), args.getString(2), args.getInt(3));
-        result = new PluginResult(Status.OK, "trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
-      } catch (Exception e) {
-        result = new PluginResult(Status.JSON_EXCEPTION);
+        tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));
+        callback.success("trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
+        return true;
+      } catch (final Exception e) {
+        callback.error(e.getMessage());
       }
     } else if (action.equals("trackPage")) {
       try {
-        tracker.trackPageView(args.getString(0));
-        result = new PluginResult(Status.OK, "trackPage - url = " + args.getString(0));
-      } catch (Exception e) {
-        result = new PluginResult(Status.JSON_EXCEPTION);
+        tracker.sendView(args.getString(0));
+        callback.success("trackPage - url = " + args.getString(0));
+        return true;
+      } catch (final Exception e) {
+        callback.error(e.getMessage());
       }
     } else if (action.equals("setVariable")) {
       try {
-        tracker.setCustomVar(args.getInt(2), args.getString(0), args.getString(1));
-        result = new PluginResult(Status.OK, "setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
-      } catch (Exception e) {
-        result = new PluginResult(Status.JSON_EXCEPTION);
+        tracker.setCustomDimension(args.getInt(0), args.getString(1));
+        callback.success("setVariable passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
+        return true;
+      } catch (final Exception e) {
+        callback.error(e.getMessage());
+      }
+    } else if (action.equals("setDimension")) {
+      try {
+        tracker.setCustomDimension(args.getInt(0), args.getString(1));
+        callback.success("setDimension passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
+        return true;
+      } catch (final Exception e) {
+        callback.error(e.getMessage());
+      }
+    } else if (action.equals("setMetric")) {
+      try {
+        tracker.setCustomMetric(args.getInt(0), args.getLong(1));
+        callback.success("setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
+        return true;
+      } catch (final Exception e) {
+        callback.error(e.getMessage());
       }
     }
-    return result;
+    return false;
   }
 }
+
