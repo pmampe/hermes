4e7ad56d136135f5ed96363cbc5f36b7376067fe
Joakim Lundin <joakim.lundin@su.se>
2013-06-27 09:41:45 +0200

    MOB-277 - Disable ajax loader

--- assets/www/spec/util/helper.js
+++ assets/www/spec/util/helper.js
@@ -36,6 +36,10 @@ afterEach(function () {
   // Override the i18n method to make sure we don't get i18n in our tests.
   $.fn.i18n = function () {
   };
+
+  // Override jqm loader to disable display of ajax loader in tests.
+  $.mobile.loading = function () {
+  };
 });
 
 beforeEach(function () {
